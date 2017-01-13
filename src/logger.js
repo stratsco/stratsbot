8e8f4ad23054264153963cd889eaebf59ebf8368
Travis Vocino <travis@vocino.com>
2017-01-13 21:35:38 +0000

    bot name

--- src/logger.js
+++ src/logger.js
@@ -5,7 +5,7 @@ import nconf from 'nconf';
 import R from 'ramda';
 
 const production = (nconf.get('NODE_ENV') === 'production');
-const config = {name: 'gravebot'};
+const config = {name: 'stratsbot'};
 if (nconf.get('SHARDING') && nconf.get('SHARD_NUMBER')) config.shard_number = nconf.get('SHARD_NUMBER');
 const logger = bunyan.createLogger(config);
 
